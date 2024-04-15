90c99f4cbc7236f7b89a5e917c566833389b8360
David Cheetham <mr-klaatu@users.noreply.github.com>
2024-04-15 22:19:46 +0100

    latest updates

---
+++ initialise.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+# This will be run by build_mymac to check for basic requirements...
+#
+#!/bin/zsh
+
+# first check for Documents and Developer Direciries
+
+
+
+ln -s /Users/david/Library/Mobile\ Documents/com~apple~CloudDocs/Documents/Developer /Users/David/Developer               
